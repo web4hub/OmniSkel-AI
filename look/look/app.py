0df18e98f1356812fab3778412907d4c7d18c326
web4 <lunghugh@gmail.com>
2026-09-23 13:36:53 +0100

    Create app.py

---
+++ look/look/app.py
@@ -0,0 +1,19 @@
+import os
+
+import falcon
+
+from .images import ImageStore
+from .images import Resource
+
+
+def create_app(image_store):
+    image_resource = Resource(image_store)
+    app = falcon.App()
+    app.add_route('/images', image_resource)
+    return app
+
+
+def get_app():
+    storage_path = os.environ.get('LOOK_STORAGE_PATH', '.')
+    image_store = ImageStore(storage_path)
+    return create_app(image_store)
